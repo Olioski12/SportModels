1ad441c0d8f4994be9e4bea9ccc266e924967f52
Oliver Papillo <oliverpapillo2@gmail.com>
2023-10-15 14:11:06 +1100

    Added Ladder Generator

--- ALeagueScraper.R
+++ ALeagueScraper.R
@@ -2,6 +2,10 @@ library(dplyr)
 library(rvest)
 library(jsonlite)
 document <- read_json("https://aleagues.com.au/ajax/get_ticker/a-leagues/xwnjb1az11zffwty3m6vn8y6,9p3nnxhdjahfn8qswpzy8oyc3/null/")
+# ALWCId <- "9p3nnxhdjahfn8qswpzy8oyc3" 
+# TourneyId <- "7p85tm8wjdbrab8yssm2ea9sk"
+# url2 <- paste0("https://apl-middleware.keepup.football/v2/fixtures?competitionId=",ALWCId,"&tournamentId=",TourneyId)
+# doc2 <- read_json(url)
 data <- document$data
 table <- data.frame()
 for(i in 1:length(data)){
@@ -17,3 +21,11 @@ for(i in 1:length(data)){
   row <- data.frame(id=id,home=home,away=away,homescore=homescore,awayscore=awayscore,startDate=startDate,comp=comp,year=year)
   table <- rbind(table,row)
 }
+
+ALW <- table %>% filter(comp == "A-League Women")
+
+Wladder <- makeLadder(ALW)
+
+ALM <- table %>% filter(comp == "A-League Men")
+
+Mladder <- makeLadder(ALM)
---
+++ LadderFunctions.R
@@ -0,0 +1,49 @@
+library(dplyr)
+
+makeLadder <- function(matches){
+  teams <- c(matches$home,matches$away)
+  teams <- unique(teams)
+  ladder <- data.frame(team = teams)
+  ladder <- ladder %>% mutate(P = 0, W = 0, L = 0, D = 0, PTS = 0, GF = 0, GA = 0,GD = 0)
+  for (i in 1:length(teams)){
+    team <- teams[i]
+    W = 0;
+    L = 0;
+    D = 0;
+    GF = 0;
+    GA = 0;
+    for(j in 1:nrow(matches)){
+      if(matches$home[j] == team){
+        hs <- matches$homescore[j]
+        as <- matches$awayscore[j]
+        GF <- GF + hs
+        GA <- GA + as
+        if(hs>as){
+          W <- W+1
+        }else if (hs==as){
+          D <- D+1
+        }else{
+          L <- L+1
+        }
+      }
+      if(matches$away[j] == team){
+        hs <- matches$homescore[j]
+        as <- matches$awayscore[j]
+        GF <- GF + as
+        GA <- GA + hs
+        if(hs>as){
+          L <- L+1
+        }else if (hs==as){
+          D <- D+1
+        }else{
+          W <- W+1
+        }
+      }
+    }
+    ladder[i,] <- c(team,W+L+D,W,L,D,W*3+D,GF,GA,GF-GA)
+  }
+  
+  ladder <- ladder[order(ladder$PTS,decreasing=TRUE),]
+  rownames(ladder) <- 1:nrow(ladder)
+  return(ladder)
+}
